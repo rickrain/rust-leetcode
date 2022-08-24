3d5c7065cf059df5443e25276c3632301b153a89
Rick Rainey <rickra@microsoft.com>
2022-08-23 21:00:49 -0500

    Final solution for prob 3 - longest substring

--- src/prob_0003_longest_substring/mod.rs
+++ src/prob_0003_longest_substring/mod.rs
@@ -1,13 +1,15 @@
-use std::collections::HashMap;
+use std::{char, collections::HashMap};
 
 pub struct Solution;
 
+// Runtime: 4 ms, faster than 80.02% of Rust online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 2.2 MB, less than 42.42% of Rust online submissions for Longest Substring Without Repeating Characters.
 
 // abcabcbb
 #[allow(dead_code)]
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut char_map = HashMap::<char, i32>::new();
+        let mut char_map: HashMap<char, i32> = HashMap::with_capacity(s.len() / 2);
         let chars = s.chars();
 
         let mut substr_start = 0;
@@ -16,23 +18,25 @@ impl Solution {
         let mut max_substr_len = 0;
         let mut curr_substr_len = 0;
 
+        let s_bytes = s.as_bytes();
+
         for (i, c) in chars.enumerate() {
             let mut counter = char_map.entry(c).or_insert(0);
             *counter += 1;
 
             if *counter > 1 {
-                if curr_substr_len > max_substr_len {
-                    max_substr_len = curr_substr_len
-                }
-                
-                // Walk-up substr_start until we find the current char 
+                max_substr_len = std::cmp::max(curr_substr_len, max_substr_len);
+
+                // Walk-up substr_start until we find the current char
                 loop {
-                    let start_char = s.chars().nth(substr_start).unwrap();
-                    counter = char_map.get_mut(&start_char).unwrap();
+                    let start_char = &s_bytes[substr_start];
+                    let x = char::from(*start_char);
+
+                    counter = char_map.get_mut(&x).unwrap();
                     *counter -= 1;
                     substr_start += 1;
-                        
-                    if start_char == c {
+
+                    if x == c {
                         break;
                     }
                 }
@@ -41,13 +45,11 @@ impl Solution {
             } else {
                 substr_end += 1;
             }
-            
+
             curr_substr_len = substr_end - substr_start;
         }
 
-        if curr_substr_len > max_substr_len {
-            max_substr_len = curr_substr_len
-        }
+        max_substr_len = std::cmp::max(curr_substr_len, max_substr_len);
 
         max_substr_len as i32
     }
@@ -75,6 +77,9 @@ mod tests {
         assert_eq!(Solution::length_of_longest_substring("".to_string()), 0);
         assert_eq!(Solution::length_of_longest_substring("ab".to_string()), 2);
         assert_eq!(Solution::length_of_longest_substring("dvdf".to_string()), 3);
-        assert_eq!(Solution::length_of_longest_substring("anviaj".to_string()), 5);
+        assert_eq!(
+            Solution::length_of_longest_substring("anviaj".to_string()),
+            5
+        );
     }
 }
