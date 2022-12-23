9a12d8ad944d7f28027f0b045fcde8959b1d47f2
Rick Rainey <rickra@microsoft.com>
2022-12-23 09:30:02 -0600

    Refactor code

--- src/prob_0005_longest_palindromic_substr/mod.rs
+++ src/prob_0005_longest_palindromic_substr/mod.rs
@@ -5,49 +5,35 @@ impl Solution {
     pub fn longest_palindrome(s: String) -> String {
         Self::find_longest_palindrome(&s).to_string()
     }
-
-    fn find_longest_palindrome(s: &str) -> &str {
+    fn expand_to_longest_palindrome(mut left_index: i32, mut right_index: usize, s: &str) -> &str {
         let mut result = "";
-        for (i, _c) in s.chars().enumerate() {
-            let mut left_index: i32 = i as i32;
-            let mut right_index = i;
-            while left_index >= 0
-                && right_index < s.len()
-                && s.chars().nth(left_index as usize) == s.chars().nth(right_index)
-            {
-                if (right_index - left_index as usize + 1) > result.len() {
-                    result = &s[left_index as usize..right_index + 1];
-                }
-                left_index -= 1;
-                right_index += 1;
+        while left_index >= 0
+            && right_index < s.len()
+            && s.chars().nth(left_index as usize) == s.chars().nth(right_index)
+        {
+            if (right_index - left_index as usize + 1) > result.len() {
+                result = &s[left_index as usize..right_index + 1];
             }
-
-            left_index = i as i32;
-            right_index = i + 1;
-            while left_index >= 0
-                && right_index < s.len()
-                && s.chars().nth(left_index as usize) == s.chars().nth(right_index)
-            {
-                if (right_index - left_index as usize + 1) > result.len() {
-                    result = &s[left_index as usize..right_index + 1];
-                }
-                left_index -= 1;
-                right_index += 1;
-            }
+            left_index -= 1;
+            right_index += 1;
         }
         result
     }
 
-    #[allow(clippy::explicit_counter_loop)]
-    fn is_palindrome(s: &str) -> bool {
-        let mut forward_index = 0;
-        for c in s.chars().rev() {
-            if c != s.chars().nth(forward_index).unwrap_or_default() {
-                return false;
+    fn find_longest_palindrome(s: &str) -> &str {
+        let mut result = &s[0..1];
+        for (i, _c) in s.chars().enumerate() {
+            let mut last_palindrome = Self::expand_to_longest_palindrome(i as i32, i as usize, s);
+            if last_palindrome.len() > result.len() {
+                result = last_palindrome;
             }
-            forward_index += 1;
+
+            last_palindrome = Self::expand_to_longest_palindrome(i as i32, i + 1_usize, s);
+            if last_palindrome.len() > result.len() {
+                result = last_palindrome;
+            }
         }
-        true
+        result
     }
 }
 
@@ -57,11 +43,6 @@ mod tests {
 
     #[test]
     fn solution_works() {
-        assert_eq!(Solution::is_palindrome("s"), true);
-        assert_eq!(Solution::is_palindrome("aba"), true);
-        assert_eq!(Solution::is_palindrome("aaaaa"), true);
-        assert_eq!(Solution::is_palindrome("adada"), true);
-
         assert_eq!(
             Solution::longest_palindrome("a".to_string()),
             "a".to_string()
@@ -112,25 +93,3 @@ mod tests {
         );
     }
 }
-
-// b a b a d d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v i d e r b
-//   a b a d d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v i d e r
-//     b a d d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v i d e
-//       a d d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v i d
-//         d d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v i
-//           d t a t t a r r a t t a t d d e t a r t r a t e e d r e d i v
-//             t a t t a r r a t t a t d d e t a r t r a t e e d r e d i
-//               a t t a r r a t t a t d d e t a r t r a t e e d r e d
-//                 t t a r r a t t a t d d e t a r t r a t e e d r e
-//                   t a r r a t t a t d d e t a r t r a t e e d r
-//                     a r r a t t a t d d e t a r t r a t e e d
-//                       r r a t t a t d d e t a r t r a t e e
-//                         r a t t a t d d e t a r t r a t e
-//                           a t t a t d d e t a r t r a t
-//                             t t a t d d e t a r t r a
-//                               t a t d d e t a r t r
-//                                 a t d d e t a r t
-//                                   t d d e t a r
-//                                     d d e t a
-//                                       d e t
-//                                         e
