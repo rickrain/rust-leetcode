ca33a5541694d67c441f614aae90bbfcb65e229b
Rick Rainey <rickrainey2@gmail.com>
2025-03-20 17:57:33 -0500

    fix: solution for prob 7 - reverse integer

--- src/prob_0007_reverse_integer/mod.rs
+++ src/prob_0007_reverse_integer/mod.rs
@@ -6,15 +6,15 @@ pub struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
-        let s = x.abs().to_string();
-        let mut result = String::with_capacity(s.len() + 1);
+        let x_str = x.unsigned_abs().to_string();
+        let mut result = String::with_capacity(x_str.len() + 1);
+
         if x < 0 {
             result.push('-');
         }
 
-        for c in s.chars().rev() {
-            result.push(c);
-        }
+        x_str.chars().rev().for_each(|c| result.push(c));
+
         result.parse::<i32>().unwrap_or_default()
     }
 }
@@ -30,5 +30,6 @@ mod tests {
         assert_eq!(Solution::reverse(120), 21);
         assert_eq!(Solution::reverse(1534236469), 0);
         assert_eq!(Solution::reverse(0), 0);
+        assert_eq!(Solution::reverse(-2147483648), 0);
     }
 }
