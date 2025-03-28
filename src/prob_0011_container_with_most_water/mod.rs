d94b6e05b8aa107c7cdaa392c6c63ef24f00ea99
Rick Rainey <rickrainey2@gmail.com>
2025-03-27 22:56:35 -0500

    final solution for prob 11 - container with most water

--- src/prob_0011_container_with_most_water/mod.rs
+++ src/prob_0011_container_with_most_water/mod.rs
@@ -1,15 +1,32 @@
+use std::cmp;
+
 pub struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        1
+        let mut left_idx = 0;
+        let mut right_idx = height.len() - 1;
+        let mut max_area = 0;
+
+        while left_idx < right_idx {
+            let gap = (right_idx - left_idx) as i32;
+            max_area = max_area.max(cmp::min(height[left_idx], height[right_idx]) * gap);
+
+            if height[left_idx] < height[right_idx] {
+                left_idx += 1;
+            } else {
+                right_idx -= 1;
+            }
+        }
+
+        max_area
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::prob_0011_container_with_most_water::{self, Solution};
+    use crate::prob_0011_container_with_most_water::Solution;
 
     #[test]
     fn solution_works() {
